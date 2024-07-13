339001dbacf810bf80a9e58534cc5210c0fc31d0
MattSchaab <schaab.matias@gmail.com>
2024-07-13 01:53:57 -0300

    Fixed plugin errors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,12 +1,1 @@
-return {
-  -- Color Scheme
-  'catppuccin/nvim',
-  config = function()
-    -- Apply colorScheme
-    vim.cmd('colorscheme catppuccin-mocha')
-
-    -- Transparency
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  end,
-}
+return { "catppuccin/nvim", lazy = true, name = "catppuccin" }
