a7278ca579991f9ba73288589692f23f2f56d3d5
MattSchaab <schaab.matias@gmail.com>
2025-05-27 17:17:53 -0300

    Added default Mason config

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,17 @@
+return {
+  "mason-org/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "dockerfile-language-server",
+      "docker-compose-language-service",
+      "marksman",
+      "gopls",
+      "pyright",
+      "typescript-language-server",
+      "lua-language-server",
+      "html-lsp",
+      "tailwindcss-language-server",
+      "prettierd",
+    },
+  },
+}
